6f18859d0eec55845a15a07c2d2f5b47c5c0c4ba
Shaik Younus <younus123shaik@gmail.com>
2025-02-16 13:26:02 +0530

    home page responsive completed

--- blocks/product-list/product-list.js
+++ blocks/product-list/product-list.js
@@ -10,16 +10,21 @@ export default async function decorate(block) {
 
     // Generate HTML for each card from JSON data
     cardsData.data.forEach(card => {
+      
         const slide = document.createElement("div");
         slide.classList.add("slide");
         slide.innerHTML = `
             <div class="card">
                 <img src="https://drive.google.com/thumbnail?id=${card.image.split('/')[5]}"/>
-                <h3>${card.name}</h3>
-
-                <p>${card.price}</p>
-               
+                <div class="card-discription">
+                <p>${card.name}</p>
+                <p><span>$</span>${Number(card.price).toFixed(2)}</p>
+                <div>
+                <button class="button-buy"><span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="addToCartButton-icon-ry1 inline stroke-white xs_hidden"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path><line x1="3" y1="6" x2="21" y2="6"></line><path d="M16 10a4 4 0 0 1-8 0"></path></svg></span></button>
+                <span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon-icon-Dp3"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></span>
+                </div>
             </div>
+        </div>
         `;
         Productcarousel.appendChild(slide);
     });
@@ -27,24 +32,32 @@ export default async function decorate(block) {
     // Initialize Slick Carousel
     $(document).ready(function(){
         $(".product-list").slick({
-            slidesToShow: 2,   // Show 3 cards at a time
-            slidesToScroll: 1,
+            slidesToShow: 5,   // Show 3 cards at a time
+            slidesToScroll: 5,
             autoplaySpeed: 2000,
-            arrows: true,
             dots: true,
             // centerMode: true,  // Enable center mode
             // centerPadding: "50px", // Adjust spacing on the sides
             responsive: [
                 {
-                    breakpoint: 768,
+                    breakpoint: 900,
                     settings: {
-                        slidesToShow: 3
+                        slidesToShow: 4,
+                        slidesToScroll: 4
+                    }
+                },
+                {
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3
                     }
                 },
                 {
                     breakpoint: 480,
                     settings: {
-                        slidesToShow: 2
+                        slidesToShow: 2,
+                        slidesToScroll: 2
                     }
                 }
             ]
