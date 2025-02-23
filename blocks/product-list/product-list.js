151ffb5e71e70b72a4ae876bd3894864a498ab44
Shaik Younus <younus123shaik@gmail.com>
2025-02-23 10:14:45 +0530

    jquery and slick is removed

--- blocks/product-list/product-list.js
+++ blocks/product-list/product-list.js
@@ -26,11 +26,7 @@ export default async function decorate(block) {
                     <div>
                         <button class="button-buy">
                             <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                    <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
-                                    <line x1="3" y1="6" x2="21" y2="6"></line>
-                                    <path d="M16 10a4 4 0 0 1-8 0"></path>
-                                </svg>
+                                ADD TO CART
                             </span>
                         </button>
                         <span>
@@ -66,7 +62,7 @@ function initializeSwiper() {
             clickable: true,
         },
         breakpoints: {
-            1000: { slidesPerView: 5, slidesPerGroup: 5 },
+            950: { slidesPerView: 5, slidesPerGroup: 5 },
             800: { slidesPerView: 4, slidesPerGroup: 4 },
             500: { slidesPerView: 3, slidesPerGroup: 3 },
             300: { slidesPerView: 2, slidesPerGroup: 2 },
