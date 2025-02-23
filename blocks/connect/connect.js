4df81989c159552b30fd348c0b1759f0b3bfde4b
Shaik Younus <younus123shaik@gmail.com>
2025-02-23 14:16:10 +0530

    subsribe form update

--- blocks/connect/connect.js
+++ blocks/connect/connect.js
@@ -1,7 +1,8 @@
 export default function decorate(block) {
     let div = document.createElement('div');
     let divHtml = `<form class="subscribe-form">
-        <input type="text" class="subscribe-input" required/>
+    <label for="subscribe"></label>
+        <input type="text" name="subscribe" class="subscribe-input" required/>
         <button class="subscribe-btn">Subscribe</button>
     </form>`
     div.innerHTML = divHtml;
