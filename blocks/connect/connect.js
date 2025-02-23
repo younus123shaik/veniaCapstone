6aeebbe15d362ba8064ee53f859acb449e1b5bd7
Shaik Younus <younus123shaik@gmail.com>
2025-02-23 14:19:31 +0530

    Update connect.js

--- blocks/connect/connect.js
+++ blocks/connect/connect.js
@@ -1,10 +1,10 @@
 export default function decorate(block) {
     let div = document.createElement('div');
     let divHtml = `<form class="subscribe-form">
-    <label for="subscribe"></label>
-        <input type="text" name="subscribe" class="subscribe-input" required/>
-        <button class="subscribe-btn">Subscribe</button>
-    </form>`
+    <label for="subscribe-input"></label>
+    <input type="email" id="subscribe-input" name="subscribe" class="subscribe-input" required placeholder="Enter your email"/>
+    <button type="submit" class="subscribe-btn">Subscribe</button>
+</form>`
     div.innerHTML = divHtml;
     block.children[1].append(div.firstElementChild)
 }
