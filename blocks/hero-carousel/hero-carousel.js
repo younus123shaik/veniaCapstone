636eee7be89d853f21792a541bef795f190eca0d
Shaik Younus <younus123shaik@gmail.com>
2025-02-24 12:43:37 +0530

    removing lazyloading from hero carousel

--- blocks/hero-carousel/hero-carousel.js
+++ blocks/hero-carousel/hero-carousel.js
@@ -1,28 +1,35 @@
 export default function decorate(block) {
-// Add Swiper classes dynamically
-block.classList.add('swiper');
-block.closest('div').firstElementChild.classList.add('hero-slider', 'swiper-slide');
-block.closest('div').lastElementChild.classList.add('hero-slider', 'swiper-slide');
 
-const wrapper = document.createElement('div');
-wrapper.classList.add('swiper-wrapper');
+  //remove lazyloading
+  block.querySelectorAll("picture img").forEach((img) => {
+    img.removeAttribute("loading");
+  });
+  // Add Swiper classes dynamically
+  block.classList.add("swiper");
+  block
+    .closest("div")
+    .firstElementChild.classList.add("hero-slider", "swiper-slide");
+  block
+    .closest("div")
+    .lastElementChild.classList.add("hero-slider", "swiper-slide");
 
-wrapper.append(block.closest('div').firstElementChild)
-wrapper.append(block.closest('div').lastElementChild)
-block.append(wrapper);
-// Add Swiper pagination & navigation elements
-const pagination = document.createElement('div');
-pagination.classList.add('swiper-pagination');
-block.appendChild(pagination);
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("swiper-wrapper");
 
-    new Swiper(".swiper", {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-    });
+  wrapper.append(block.closest("div").firstElementChild);
+  wrapper.append(block.closest("div").lastElementChild);
+  block.append(wrapper);
+  // Add Swiper pagination & navigation elements
+  const pagination = document.createElement("div");
+  pagination.classList.add("swiper-pagination");
+  block.appendChild(pagination);
+
+  new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 }
-
-
